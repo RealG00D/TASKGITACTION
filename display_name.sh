c8291ee755355a46dbdd59172f496e6b9a246fd5
RealG00D <146886024+RealG00D@users.noreply.github.com>
2023-11-09 21:49:50 +0200

    Update display_name.sh

--- display_name.sh
+++ display_name.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Замініть на ваше ім'я та прізвище
-first_name="ВашеІм'я"
-last_name="ВашеПрізвище"
+first_name="Олег"
+last_name="Деревецький"
 
 echo "Ім'я: $first_name"
 echo "Прізвище: $last_name"
